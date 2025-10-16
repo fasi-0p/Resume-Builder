e9fc4efbb6497d691083382ec1a422362781b5ba
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2025-10-16 19:04:03 +0530

    done with login page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {PlusIcon} from 'lucide-react';
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>
-        Dashboard Page
-      </h1>
+      <div className='max-w-7xl mx-auto px-4 py-8'>
+      <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, Joe</p>
+        <div className='flex gap-4'>
+          <button>
+            <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full'/>
+            <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
